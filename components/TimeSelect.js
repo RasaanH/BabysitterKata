377c11bb309578ec2d16c55eb04d3a270ff41321
RasaanH <Rasaan@resolvve.com>
2021-05-26 03:39:38 -0400

    abstracted rates list out to its own function to avoid duplicate code

--- components/TimeSelect.js
+++ components/TimeSelect.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Picker } from 'react-native-woodpicker';
+import ratesReturn from '../functions/ratesReturn';
 import {
     StyleSheet,
     View,
@@ -7,20 +8,7 @@ import {
   } from 'react-native';
 
   const TimeSelect = (props) => {
-    const timeRates = [
-        {label: "5:00 p.m.", index: 0, value: 17, rate: 12},
-        {label: "6:00 p.m.", index: 1, value: 18, rate: 12},
-        {label: "7:00 p.m.", index: 2, value: 19, rate: 12},
-        {label: "8:00 p.m.", index: 3, value: 20, rate: 12},
-        {label: "9:00 p.m.", index: 4, value: 21, rate: 8},
-        {label: "10:00 p.m.", index: 5, value: 22,  rate: 8},
-        {label: "11:00 p.m.", index: 6, value: 23, rate: 8},
-        {label: "12:00 a.m.", index: 7, value: 0, rate: 16},
-        {label: "1:00 a.m.", index: 8, value: 1, rate: 16},
-        {label: "2:00 a.m.", index: 9, value: 2, rate: 16},
-        {label: "3:00 a.m.", index: 10, value: 3, rate: 16},
-        {label: "4:00 a.m.", index: 11, value: 4, rate: 0}
-    ]
+    const timeRates = ratesReturn();
     const timeRatesEnd = [...timeRates]
     timeRatesEnd.shift(); //so you can't choose to end at 5:00 p.m.
     const timeRatesStart = [...timeRates]
